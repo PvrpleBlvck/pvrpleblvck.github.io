ac2bd3e4d7583471565b3031e78b77f8c56c02ef
Norest Nyamhunga <41530377+PvrpleBlvck@users.noreply.github.com>
2022-02-09 10:21:42 +0200

    Update pvrple.js

--- tradingview/layout/js/pvrple.js
+++ tradingview/layout/js/pvrple.js
@@ -1,4 +1,4 @@
-/ function aaa() {
+//function aaa() {
 //   "use strict";
 //   var t;
 //   var e = ((t = {})["color-brand"] = "#2196f3", t["color-gull-gray"] = "#9db2bd", t["color-scooter"] = "#38acdb", t["color-curious-blue"] = "#299dcd", t);
@@ -319,3 +319,72 @@
 //     }, Object.defineProperties(e.prototype, i), e
 //   }(c))
 // }();
+
+
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.investagrams.com/Chart/PSE:*
+// @icon         https://www.google.com/s2/favicons?domain=investagrams.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     setInterval(function(){
+         $("div[data-ng-controller=intrusiveShowmanUserControlController]").css("display", "none");
+         $("div[data-ng-controller=popupAdsUserControlController]").css("display", "none");
+         $("div.modal-backdrop").css("display", "none");
+         console.log("hide ads");
+     }, 2000);
+
+})();
+
+/* ================================================================= */
+
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.investagrams.com/Stock/*
+// @icon         https://www.google.com/s2/favicons?domain=investagrams.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+     setInterval(function(){
+         $("div[data-ng-controller=intrusiveShowmanUserControlController]").css("display", "none");
+         $("div[data-ng-controller=popupAdsUserControlController]").css("display", "none");
+         $("div.modal-backdrop").css("display", "none");
+         console.log("hide ads");
+     }, 2000);
+
+})();
+
+
+/* ================================================================= */
+
+
+/* Greasemonkey script */
+ /* This script will hide the TradingView logo or any custom logo */
+ setInterval(function() { 
+   /* hide the logo */
+     document.getElementsByTagName("iframe")[0].contentWindow.document.querySelector('div > .onchart-custom-logo').innerHTML="";
+   /* hide the ads */  
+   document.getElementById("toast-container").style.visibility="hidden";
+ }, 1000);
+
+
+/* hide investagram popup */
+setInterval(function() {
+ $("div[data-ng-controller=popupAdsUserControlController]").css("display", "none")
+}, 1000);
+
+
