8690816bdf6035389646c5dd6e54a66e5bbea37a
Norest Nyamhunga <41530377+PvrpleBlvck@users.noreply.github.com>
2022-02-10 08:33:41 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Forex Enthusiast", "Artist", "Musician", "Freelancer"],
+        strings: ["A Developer", "A Forex Enthusiast", "An Artist", "A Music Producer", "A Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Forex Enthusiast", "Artist", "Musician", "Freelancer"],
+        strings: ["A Developer", "A Forex Enthusiast", "An Artist", "A Music Producer", "A Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
